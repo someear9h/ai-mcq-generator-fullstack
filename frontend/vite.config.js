ecb53c104c8d33d7dcc87a361889f5fad8a93357
someear9h <tikotkarsamarth@gmail.com>
2025-07-17 00:21:39 +0530

    requierements and front end modification

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const isDev = env.VITE_DEBUG === "true";
+
+  return {
+    plugins: [react()],
+    server: {
+      ...(isDev && {
+        proxy: {
+          "/api": {
+            target: "http://localhost:8000", // FastAPI backend
+            changeOrigin: true,
+            secure: false
+          }
+        }
+      })
+    },
+    define: {
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
+    }
+  };
+});
