d0d39bf9933de8b84fb5d2579e31d0d06e6f23e0
someear9h <tikotkarsamarth@gmail.com>
2025-07-17 01:21:50 +0530

    building front end

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// define config
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
